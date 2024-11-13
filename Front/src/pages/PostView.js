cf48539e6a925e2549970fe5ce0f95cc510d5a6b
leenaaq <v584731@gmail.com>
2024-11-14 08:04:01 +0900

    1

--- Front/src/pages/PostView.js
+++ Front/src/pages/PostView.js
@@ -398,7 +398,6 @@ const PostView = () => {
                             alt={`${comment.authorNickname} 프로필`}
                             className="PostView-comment-profile-image"
                         />
-                        <p className="PostView-comment-userid">{comment.authorUserId}</p>
                         <p className="PostView-comment-nickname">{comment.authorNickname}</p>
                         <p className="PostView-comment-date">
                           {new Date(comment.date).toLocaleDateString()} {/* 작성일자 표시 */}
