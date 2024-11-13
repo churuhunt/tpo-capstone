f7322687b5b1e5f5a074a12ea0d7cb7113cc360d
Your Name <you@example.com>
2024-11-14 02:36:03 +0900

    댓글 엔티티 수정

--- src/main/java/tpo/capstone/dto/CommentResponse.java
+++ src/main/java/tpo/capstone/dto/CommentResponse.java
@@ -15,8 +15,9 @@ import java.util.Date;
 public class CommentResponse {
     private Long id;
     private String content;
+    private String authorUserId;
     private String authorNickname;
-    private String authorProfileImageUrl;  // 작성자 프로필 이미지 URL 추가
+    private String authorProfileImageUrl;
     private Date date;
     private int likes;
     private int dislikes;
@@ -25,8 +26,9 @@ public class CommentResponse {
     public CommentResponse(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
+        this.authorUserId = comment.getAuthor().getUserId();
         this.authorNickname = comment.getAuthor().getNickname();
-        this.authorProfileImageUrl = comment.getAuthor().getProfileImageUrl(); // 작성자의 프로필 이미지 URL 설정
+        this.authorProfileImageUrl = comment.getAuthor().getProfileImageUrl();
         this.date = comment.getDate();
         this.likes = comment.getLikes();
         this.dislikes = comment.getDislikes();
