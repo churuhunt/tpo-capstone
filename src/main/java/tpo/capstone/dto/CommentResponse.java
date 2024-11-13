69802864bceee528af8f2465cc3488c296ef44df
Your Name <you@example.com>
2024-11-14 07:31:27 +0900

    댓글 dto 수정

--- src/main/java/tpo/capstone/dto/CommentResponse.java
+++ src/main/java/tpo/capstone/dto/CommentResponse.java
@@ -15,7 +15,7 @@ import java.util.Date;
 public class CommentResponse {
     private Long id;
     private String content;
-    private String authorUserId;
+    private Long authorId; // Long 타입으로 변경
     private String authorNickname;
     private String authorProfileImageUrl;
     private Date date;
@@ -26,7 +26,7 @@ public class CommentResponse {
     public CommentResponse(Comment comment) {
         this.id = comment.getId();
         this.content = comment.getContent();
-        this.authorUserId = comment.getAuthor().getUserId();
+        this.authorId = comment.getAuthor().getId(); // authorUserId를 authorId로 변경하고 Long id 값 사용
         this.authorNickname = comment.getAuthor().getNickname();
         this.authorProfileImageUrl = comment.getAuthor().getProfileImageUrl();
         this.date = comment.getDate();
